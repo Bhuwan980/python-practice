945a316ee0377cd378634ec32b87fa5eebd148b0
bhuwan neupane <bdr.bhuwan@gmail.com>
2019-12-31 09:47:05 +0545

    decoder is done in python

---
+++ decorators.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 31 09:00:08 2019
+
+@author: cousi
+"""
+
+
+"""
+def my_dec(fun):
+    def wrapper(i,j):
+        print("your age is: ")
+        print("your age is: ",fun(i,j))
+        print("after calculating your age.")
+    return wrapper
+@my_dec
+def sum(i,j):
+    return i+j
+print(sum(10,20))
+
+
+"""
+
+"""
+def mcu():
+
+def dceu():"""
+    
+def userinput():
+    power = int(input("please enter your power level at range 1-10: "))
+    if power > 10:
+        print("you entered wrong amount of power.")
+        power = int(input("please enter your power level at range 1-10: "))
+userinput()
+        
